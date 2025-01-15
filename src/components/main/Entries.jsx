eb18c251d67f2fa23242058a09585248eb03cd04
nikola <nikola88393@gmail.com>
2025-01-15 22:15:28 +0200

    Connect add and delete record to backend api

--- src/components/main/Entries.jsx
+++ src/components/main/Entries.jsx
@@ -18,166 +18,116 @@ import { useDisclosure } from "@mantine/hooks";
 import { useEffect, useState } from "react";
 import { modals } from "@mantine/modals";
 import LoadingElement from "../common/LoadingElement";
-// import useAxiosPrivate from "../../hooks/useAxiosPrivate";
+import useAxiosPrivate from "../../hooks/useAxiosPrivate";
+import { useForm } from "@mantine/form";
 
-let addressBookData = [
-  {
-    id: 1,
-    firstName: "Иван",
-    lastName: "Петров",
-    companyName: "Петров Логистика",
-    address: "гр. София, ул. 'Иван Вазов' 15",
-    phoneNumber: "02/1234567",
-    emailAddress: "ivan.petrov@email.com",
-    faxNumber: "02/7654321",
-    mobilePhoneNumber: "0888123456",
-    comment: "Основен контакт за логистика",
-    badge: "Логистика",
-    badgeColor: "blue",
-    additionalFields: [
-      { label: "Длъжност", value: "Директор" },
-      { label: "Сайт", value: "www.petrovlogistics.com" },
-      { label: "Допълнително", value: "Записан на 2 места" },
-    ],
-  },
-  {
-    id: 2,
-    firstName: "Мария",
-    lastName: "Иванова",
-    companyName: "Детски свят",
-    address: "гр. Пловдив, ул. 'Марица' 23",
-    phoneNumber: "032/223344",
-    emailAddress: "maria.ivanova@email.bg",
-    faxNumber: "032/443322",
-    mobilePhoneNumber: "0899123456",
-    comment: "Клиент за играчки",
-    badge: "Детски магазин",
-    badgeColor: "green",
-  },
-  {
-    id: 3,
-    firstName: "Георги Георги Георги",
-    lastName: "Димитров",
-    companyName: "Димитров ЕООД",
-    address: "гр. Варна, бул. 'Черно море' 12",
-    phoneNumber: "052/334455",
-    emailAddress: "georgi.dimitrov@mail.bg",
-    faxNumber: "052/554433",
-    mobilePhoneNumber: "0878123456",
-    comment: "Доставчик на офис консумативи",
-    badge: "Доставчик",
-    badgeColor: "red",
-  },
-  {
-    id: 4,
-    firstName: "Елена",
-    lastName: "Костова",
-    companyName: "Костова Стил",
-    address: "гр. Бургас, ул. 'Александровска' 48",
-    phoneNumber: "056/112233",
-    emailAddress: "elena.kostova@mail.com",
-    faxNumber: "056/332211",
-    mobilePhoneNumber: "0886123456",
-    comment: "Контакт за дизайнерски услуги",
-    badge: "Дизайнер",
-    badgeColor: "purple",
-  },
-  {
-    id: 5,
-    firstName: "Николай",
-    lastName: "Тодоров",
-    companyName: "Тодоров Имоти",
-    address: "гр. Русе, ул. 'Дунав' 10",
-    phoneNumber: "082/445566",
-    emailAddress: "nikolay.todorov@mail.bg",
-    faxNumber: "082/665544",
-    mobilePhoneNumber: "0885123456",
-    comment: "Брокер на недвижими имоти",
-    badge: "Брокер",
-    badgeColor: "orange",
-  },
-];
 const Entries = () => {
   const [opened, { open, close }] = useDisclosure(false);
   const [activeEntry, setActiveEntry] = useState(null);
   const [addressBook, setAddressBook] = useState([]);
   const [refetechTrigger, setRefetechTrigger] = useState(0);
   const [isLoading, setIsLoading] = useState(false);
-  // const axiosPrivate = useAxiosPrivate();
+  const [customFields, setCustomFields] = useState([]);
+  const axiosPrivate = useAxiosPrivate();
 
+  const addForm = useForm({
+    mode: "uncontrolled",
+    validate: {
+      firstName: (value) =>
+        value === undefined ? "Името е задължително" : null,
+      lastName: (value) =>
+        value === undefined ? "Фамилията е задължителна" : null,
+      companyName: (value) =>
+        value === undefined ? "Фирмата е задължителна" : null,
+      address: (value) =>
+        value === undefined ? "Адресът е задължителен" : null,
+      phoneNumber: (value) =>
+        value === undefined ? "Телефонът е задължителен" : null,
+      email: (value) => (value === undefined ? "Имейлът е задължителен" : null),
+      faxNumber: (value) =>
+        value === undefined ? "Факсът е задължителен" : null,
+      mobilePhoneNumber: (value) =>
+        value === undefined ? "Мобилният е задължителен" : null,
+      comment: (value) =>
+        value === undefined ? "Коментарът е задължителен" : null,
+    },
+  });
+
+  // Fetch data from the server
   useEffect(() => {
     setIsLoading(true);
     const getData = async () => {
       try {
-        // make api call to get data
-        // const response = await axiosPrivate.get('/addressbook');
-        //  setAddressBook(response.data);
-        setAddressBook(addressBookData);
+        const response = await axiosPrivate.get("api/user-record");
+        console.log(response);
+        setAddressBook(response.data);
       } catch (error) {
         console.error(error);
       } finally {
         setIsLoading(false);
       }
     };
-
     setTimeout(() => {
       getData();
-    }, 2000);
-  }, [refetechTrigger]);
+    }, 1000);
+  }, [refetechTrigger, axiosPrivate]);
 
-  const handleOpen = (entry) => {
-    console.log(entry);
-    setActiveEntry(entry);
+  const handleOpen = () => {
+    // console.log(entry);
+    // setActiveEntry(entry);
     open();
   };
 
-  // const handleInputChange = (field, value) => {
-  //   setActiveEntry((prev) => ({ ...prev, [field]: value }));
-  // };
+  // Add record
+  const handleAddRecord = async (values) => {
+    const customFieldValues = customFields.reduce((acc, field) => {
+      acc[field.name] = values[field.name];
+      return acc;
+    }, {});
 
-  const updateEntry = () => {
-    setAddressBook((prev) =>
-      prev.map((entry) =>
-        entry.id === activeEntry.id ? { ...activeEntry } : entry
-      )
-    );
-    // setRefetechTrigger((prev) => prev + 1);
-    close();
-  };
+    const payload = { ...values, ...customFieldValues };
 
-  const deleteEntry = (id) => {
-    setAddressBook((prev) => prev.filter((entry) => entry.id !== id));
-    // setRefetechTrigger((prev) => prev + 1);
-  };
-
-  const handleInputChange = (field, value, index = null) => {
-    if (index !== null) {
-      setActiveEntry((prev) => {
-        const updatedFields = [...prev.additionalFields];
-        updatedFields[index].value = value;
-        return { ...prev, additionalFields: updatedFields };
-      });
-    } else {
-      setActiveEntry((prev) => ({ ...prev, [field]: value }));
+    try {
+      const response = await axiosPrivate.post("api/user-record", payload);
+      console.log(response.data);
+      setRefetechTrigger((prev) => prev + 1);
+    } catch (error) {
+      console.error(error);
     }
   };
 
-  const addCustomField = () => {
-    setActiveEntry((prev) => ({
-      ...prev,
-      additionalFields: [
-        ...(prev.additionalFields || []),
-        { label: "Ново поле", value: "" },
-      ],
-    }));
+  // Delete record
+  const handleDeleteRecord = async (id) => {
+    try {
+      const response = await axiosPrivate.delete("api/user-record/delete", {
+        data: { recordId: id },
+      });
+      console.log(response.data);
+      setRefetechTrigger((prev) => prev + 1);
+    } catch (error) {
+      console.error(error);
+    }
   };
 
-  const deleteCustomField = (index) => {
-    setActiveEntry((prev) => {
-      const updatedFields = [...prev.additionalFields];
-      updatedFields.splice(index, 1);
-      return { ...prev, additionalFields: updatedFields };
-    });
+  const handleAddCustomField = () => {
+    setCustomFields((prev) => [
+      ...prev,
+      { id: Date.now(), name: "", label: "", type: "text" },
+    ]);
+  };
+
+  const handleCustomFieldChange = (id, field, value) => {
+    setCustomFields((prev) =>
+      prev.map((customField) =>
+        customField.id === id ? { ...customField, [field]: value } : customField
+      )
+    );
+  };
+
+  const handleRemoveCustomField = (id) => {
+    setCustomFields((prev) =>
+      prev.filter((customField) => customField.id !== id)
+    );
   };
 
   return (
@@ -221,8 +171,8 @@ const Entries = () => {
         </Flex>
       </Flex>
       <Modal opened={opened} onClose={close} title="Детайли за контакт">
-        {activeEntry ? (
-          <div>
+        <div>
+          <form onSubmit={addForm.onSubmit(handleAddRecord)}>
             <Tabs defaultValue="details" variant="outline">
               <Tabs.List grow>
                 <Tabs.Tab value="details">Детайли</Tabs.Tab>
@@ -231,109 +181,111 @@ const Entries = () => {
               <Tabs.Panel value="details">
                 <TextInput
                   label="Име"
-                  value={activeEntry.firstName}
-                  onChange={(e) =>
-                    handleInputChange("firstName", e.target.value)
-                  }
+                  key={addForm.key("firstName")}
+                  {...addForm.getInputProps("firstName")}
                 />
                 <TextInput
                   label="Фамилия"
-                  value={activeEntry.lastName}
-                  onChange={(e) =>
-                    handleInputChange("lastName", e.target.value)
-                  }
+                  key={addForm.key("lastName")}
+                  {...addForm.getInputProps("lastName")}
                 />
                 <TextInput
                   label="Фирма"
-                  value={activeEntry.companyName}
-                  onChange={(e) =>
-                    handleInputChange("companyName", e.target.value)
-                  }
+                  key={addForm.key("companyName")}
+                  {...addForm.getInputProps("companyName")}
                 />
                 <TextInput
                   label="Адрес"
-                  value={activeEntry.address}
-                  onChange={(e) => handleInputChange("address", e.target.value)}
+                  key={addForm.key("address")}
+                  {...addForm.getInputProps("address")}
                 />
                 <TextInput
                   label="Телефон"
-                  value={activeEntry.phoneNumber}
-                  onChange={(e) =>
-                    handleInputChange("phoneNumber", e.target.value)
-                  }
+                  key={addForm.key("phoneNumber")}
+                  {...addForm.getInputProps("phoneNumber")}
                 />
                 <TextInput
                   label="Имейл"
-                  value={activeEntry.emailAddress}
-                  onChange={(e) =>
-                    handleInputChange("emailAddress", e.target.value)
-                  }
+                  key={addForm.key("email")}
+                  {...addForm.getInputProps("email")}
                 />
                 <TextInput
                   label="Факс"
-                  value={activeEntry.faxNumber}
-                  onChange={(e) =>
-                    handleInputChange("faxNumber", e.target.value)
-                  }
+                  key={addForm.key("faxNumber")}
+                  {...addForm.getInputProps("faxNumber")}
                 />
                 <TextInput
                   label="Мобилен"
-                  value={activeEntry.mobilePhoneNumber}
-                  onChange={(e) =>
-                    handleInputChange("mobilePhoneNumber", e.target.value)
-                  }
+                  key={addForm.key("mobilePhoneNumber")}
+                  {...addForm.getInputProps("mobilePhoneNumber")}
                 />
                 <TextInput
                   label="Коментар"
-                  value={activeEntry.comment}
-                  onChange={(e) => handleInputChange("comment", e.target.value)}
+                  key={addForm.key("comment")}
+                  {...addForm.getInputProps("comment")}
                 />
-                <Button onClick={updateEntry} mt="md">
+                <Button type="submit" mt="md">
                   Запази
                 </Button>
               </Tabs.Panel>
               <Tabs.Panel value="additional">
-                <Button onClick={addCustomField}>Добави поле +</Button>
-                {activeEntry.additionalFields &&
-                  activeEntry.additionalFields.map((field, index) => (
-                    <Fieldset mt="md" mb="md" key={index}>
-                      <TextInput
-                        label="Етикет"
-                        value={field.label}
-                        onChange={(e) =>
-                          handleInputChange("label", e.target.value, index)
-                        }
-                      />
-                      <TextInput
-                        label="Стойност"
-                        value={field.value}
-                        onChange={(e) =>
-                          handleInputChange("value", e.target.value, index)
-                        }
-                      />
-                      <Group mt="md" grow>
-                        <Button
-                          color="red"
-                          size="xs"
-                          onClick={() => deleteCustomField(index)}
-                        >
-                          Изтрий
-                        </Button>
-                        <Button size="xs">Запази</Button>
-                      </Group>
-                    </Fieldset>
-                  ))}
+                {customFields.map((field) => (
+                  <Flex key={field.id} gap="sm" mb="sm">
+                    <TextInput
+                      placeholder="Име на полето"
+                      value={field.name}
+                      onChange={(e) =>
+                        handleCustomFieldChange(
+                          field.id,
+                          "name",
+                          e.target.value
+                        )
+                      }
+                    />
+                    <TextInput
+                      placeholder="Етикет на полето"
+                      value={field.label}
+                      onChange={(e) =>
+                        handleCustomFieldChange(
+                          field.id,
+                          "label",
+                          e.target.value
+                        )
+                      }
+                    />
+                    <NativeSelect
+                      data={["text", "number", "email", "date"]}
+                      value={field.type}
+                      onChange={(e) =>
+                        handleCustomFieldChange(
+                          field.id,
+                          "type",
+                          e.target.value
+                        )
+                      }
+                    />
+                    <Button
+                      color="red"
+                      onClick={() => handleRemoveCustomField(field.id)}
+                    >
+                      X
+                    </Button>
+                  </Flex>
+                ))}
+                <Button onClick={handleAddCustomField} mt="sm">
+                  Добави поле +
+                </Button>
               </Tabs.Panel>
             </Tabs>
-          </div>
-        ) : (
-          <p>Няма избрани данни.</p>
-        )}
+          </form>
+        </div>
       </Modal>
       <Divider my="sm" />
       <Stack>
-        {addressBook.map((entry) => {
-          return (
+        {addressBook.length === 0 ? (
+          <div>Няма записи</div>
+        ) : (
+          addressBook.map((entry) => (
             <Box pos="relative" p={10} key={entry.id}>
               {entry.badge && (
                 <Badge
@@ -356,7 +308,7 @@ const Entries = () => {
                 >
                   <Flex direction={{ base: "column", md: "row" }} gap="xl">
                     <span>{entry.firstName + " " + entry.lastName}</span>
-                    <span>{entry.emailAddress}</span>
+                    <span>{entry.email}</span>
                     <span>{entry.phoneNumber}</span>
                   </Flex>
                   <Flex direction={{ base: "row", xl: "column" }} gap="xs">
@@ -372,7 +324,7 @@ const Entries = () => {
                           labels: { cancel: "Отказ", confirm: "Изтрий" },
                           confirmProps: { color: "red" },
                           onConfirm: () => {
-                            deleteEntry(entry.id);
+                            handleDeleteRecord(entry.id);
                             modals.close();
                           },
                           onCancel: () => modals.close(),
@@ -385,8 +337,8 @@ const Entries = () => {
                 </Flex>
               </Card>
             </Box>
-          );
-        })}
+          ))
+        )}
       </Stack>
       <Pagination w="fit-content" mr="auto" ml="auto" mt="lg" />
     </Box>
